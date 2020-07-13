6323ca20b9f6125f6aae32e21ccb3f81cbe05985
e-adachi-55 <64426498+e-adachi-55@users.noreply.github.com>
2020-07-14 01:37:44 +0900

    for commit
    レスポンシブ・モーダル部分完成

--- js/main.js
+++ js/main.js
@@ -15,3 +15,53 @@
 		return false;
 	});
 
+	$(window).on("scroll", function() {
+		//フェードイン
+		$('[data-fadeIn]').each(function(index, el) {
+			if( $(window).scrollTop() > ( $(el).offset().top - $(window).height() / 2 ) ){
+				$(el).addClass('is-over');
+			}
+		});
+	});
+
+	$(function() {
+        //スムーズスクロール
+        let runScroll = function() {
+            $('a[href^="#"]').click(function() {
+                let speed = 400;
+                let href = $(this).attr("href");
+                let $target = $(href == "#" || href == "" ? 'html' : href);
+                let position = $target.offset().top - 100;
+
+                $('body,html').animate({
+                    scrollTop: position
+                }, speed, 'swing');
+                return false;
+            });
+        };
+
+        // ハンバーガーメニューチェックボックス外す処理
+        let unlockCheckBox = function() {
+            let $trigger = $('.header__navi a');
+            let $target = $('input[type="checkbox"]');
+
+            $trigger.click(function() {
+                $target.prop('checked', false);
+            });
+        };
+		// Document Ready
+            runScroll();
+            unlockCheckBox();
+
+		//モーダルを表示したときの背景を固定する
+		var scrollPosition;
+		$('.js-modal-open').on("click", function() {
+			scrollPosition = $(window).scrollTop();
+			$('body').addClass('fixed').css({'top': -scrollPosition});
+		});
+		$('.js-modal-close').on("click", function() {
+			$('body').removeClass('fixed').css({'top': 0});
+			window.scrollTo( 0 , scrollPosition );
+		});	
+    });
+
